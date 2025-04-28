f2f97d718d6302bee46b5f900a52098c778696a3
sudhanshu248 <sainisudhanshu389@gmail.com>
2025-04-28 19:24:49 +0530

    Add login Frontend

---
+++ frontend/src/landing/Notfound.jsx
@@ -0,0 +1,13 @@
+export default function NotFound(){
+    return(
+        <>
+            <div className="container text-center">
+                <div className="row">
+                    <div style={{fontSize: "18.9vh"}}>404</div>
+                    <h1 className="p-2">Sorry, Page Not Found !</h1>
+                    <p className="pb-5">The page you requested could not be found.</p>
+                </div>
+            </div>
+        </>
+    )
+}
