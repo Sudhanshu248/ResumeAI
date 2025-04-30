8e4e06e0d4d6ea946578a24e2985b7da51afa8da
Himanshu-kumar025 <hk1832141@gmail.com>
2025-04-30 19:52:02 +0530

    changes in userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,13 +1,8 @@
 const express = require("express");
 const { signin, login } = require("../controllers/userController.js");
-const auth = require("../middleware/auth");
+
 
 const router = express.Router();
-
-// Test endpoint to verify token
-router.get('/test-auth', auth, (req, res) => {
-    res.json({ message: 'Token is valid', userId: req.user.userId });
-});
 
 router.route('/signin').post(signin);
 router.route('/login').post(login);
