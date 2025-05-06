c495d0bf6631a39437efce8278050edefa99dcac
Sudhanshu Saini <sainisudhanshu389@gmail.com>
2025-05-06 21:32:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:3002', 'https://resumeai-itv1.onrender.com'],
+    origin: ['https://resumeai-itv1.onrender.com'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
