e52f8b60bbe5836a3af37b966954b575d76fd080
Himanshu-kumar025 <hk1832141@gmail.com>
2025-07-24 15:07:02 +0530

    add delete button

--- backend/routes/resume.route.js
+++ backend/routes/resume.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import {  createResume,  updateResume , allResumes} from '../controllers/resume.controller.js';
+import { createResume, updateResume, allResumes, deleteResume } from '../controllers/resume.controller.js';
 
 const router = express.Router();
 
 router.post("/create-resume", createResume);
 router.put("/update-resume/:id", updateResume); //  corrected
 router.get("/all-resumes", allResumes);
+router.delete("/delete-resume/:id", deleteResume);
 
 export default router;
